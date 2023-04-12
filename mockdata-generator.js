87400defccf9f1fb1c2670a762f536c6252ca99f
kyemets <yemetskonstantin@gmail.com>
2023-04-12 21:21:10 +0200

    fixed bugs

--- mockdata-generator.js
+++ mockdata-generator.js
@@ -8,31 +8,30 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/todos', (req, res) => {
+app.get('/notes', (req, res) => {
   const count = parseInt(req.query.count) || 0;
   if (count <= 1 || count > 500) {
     res.status(400).json({ error: 'Invalid count parameter' });
     return;
   }
 
-  const vehicles = [];
+  const notes = [];
 
   for (let i = 0; i < count; i++) {
 
-    // generation date
     const date = new Date(Date.now() + i * 1000 * 60 * 60 * 24);
 
-    const todo = {
+    const note = {
       id: i + 1,
       date: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
-      title: `${i + 1}-todo`,
+      title: `${i + 1}-note`,
       description: `Description ${i + 1}`,
      
     };
-    vehicles.push(todo);
+    notes.push(note);
   }
 
-  res.json(vehicles);
+  res.json(notes);
 });
 
 app.listen(port, () => {
--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,26 @@
   <div class='m-5'>
     <h1 class="text-5xl mb-4 font-extrabold dark:text-white">Create notes</h1>
 
-      <label for="todosNumber">
+      <label for="notesNumber">
         <small class="ml-0 font-semibold text-gray-500 dark:text-gray-400"
-          >Number of todos to generate (min2/max500):
+          >Number of notes to generate (min2/max500):
         </small>
       </label>
 
       <input 
         type="text" 
-        id="todosNumber" 
+        id="notesNumber" 
         class="block w-full p-2 z-10 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4" 
-        v-model.number="numberOfTodos" 
+        v-model.number="numberOfNotes" 
         min="2" 
         max="500" 
         required
       >
 
-      <div v-if="numberOfTodos">
+      <div v-if="numberOfNotes">
         <button 
           type="button" 
-          class="text-white bg-blue-400 dark:bg-blue-500 cursor font-medium rounded-lg text-sm px-5 py-2.5 text-center" @click="generateTodos"
+          class="text-white bg-blue-400 dark:bg-blue-500 cursor font-medium rounded-lg text-sm px-5 py-2.5 text-center" @click="generateNotes"
           >Generate Notes
         </button>
       </div>
@@ -72,7 +72,7 @@ export default {
 
     return {
       notes: [],
-      numberOfTodos: '',
+      numberOfNotes: '',
       notify ,
     }
   },
@@ -116,16 +116,16 @@ export default {
       })
     },
 
-    generateTodos() {
+    generateNotes() {
       axios
-        .get(`http://localhost:3000/todos?count=${this.numberOfTodos}`)
+        .get(`http://localhost:3000/notes?count=${this.numberOfNotes}`)
         .then((response) => {
           this.notes = response.data;
           console.log(this.notes);
         })
         .catch(() => {
           this.showMessage = true;
-          this.message = 'Error generating todos';
+          this.message = 'Error generating notes';
         });
 
         toast("The notes were successfully generated!", {
